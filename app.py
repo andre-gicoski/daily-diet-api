6fca63aae24d83ba6c481007f45b494e2a7b9cc7
André Gicoski <andregicoskisz@gmail.com>
2025-02-07 10:48:36 -0300

    Create, readForId and readAll

--- app.py
+++ app.py
@@ -1,12 +1,88 @@
 from flask import Flask, request, jsonify
 from models.meal import Meal
 from database import db
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "your_secret_key"
+app.config["SECRET_KEY"] = "your_secret_key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///daily_diet.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db.init_app(app)
 
 
+@app.route("/meal", methods=["POST"])
+def create_meal():
+    data = request.get_json()
 
-if __name__ == '__main__':
+    name = data.get("name")
+    description = data.get("description")
+    date_time = data.get("date_time")
+    within_diet = data.get("within_diet")
+
+    if name and description and date_time and within_diet is not None:
+        try:
+            date_time = datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            return (
+                jsonify(
+                    {
+                        "message": "Formato de data inválido. Use o formato 'YYYY-MM-DD HH:MM:SS'."
+                    }
+                ),
+                400,
+            )
+
+        new_meal = Meal(
+            name=name,
+            description=description,
+            date_time=date_time,
+            within_diet=within_diet,
+        )
+
+        db.session.add(new_meal)
+        db.session.commit()
+
+        return jsonify({"message": "Refeição cadastrada com sucesso"}), 201
+
+    return jsonify({"message": "Dados inválidos"}), 400
+
+
+@app.route("/meal", methods=["GET"])
+def get_all_meals():
+    meals = Meal.query.all()
+
+    if meals:
+        return jsonify(
+            [
+                {
+                    "name": meal.name,
+                    "description": meal.description,
+                    "date_time": meal.date_time.strftime("%Y-%m-%d %H:%M:%S"),
+                    "within_diet": meal.within_diet,
+                }
+                for meal in meals
+            ]
+        )
+
+    return jsonify({"message": "Nenhuma refeição encontrada"}), 404
+
+
+@app.route("/meal/<int:id_meal>", methods=["GET"])
+def read_meal(id_meal):
+    meal_data = Meal.query.get(id_meal)
+    if meal_data:
+        return jsonify(
+            {
+                "name": meal_data.name,
+                "description": meal_data.description,
+                "date_time": meal_data.date_time.strftime("%Y-%m-%d %H:%M:%S"),
+                "within_diet": meal_data.within_diet,
+            }
+        )
+
+    return jsonify({"message": "Refeição não encontrada"}), 404
+
+
+if __name__ == "__main__":
     app.run(debug=True)
--- models/meal.py
+++ models/meal.py
@@ -1,10 +1,10 @@
 
 from database import db
-from datetime import datetime, timezone
+from datetime import datetime
 
 class Meal(db.Model):
     id = db.Column(db.Integer, primary_key=True)  
     name = db.Column(db.String(100), nullable=False) 
     description = db.Column(db.Text, nullable=True) 
-    date_time = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
-    within_diet = db.Column(db.Boolean, nullable=False) 
+    date_time = db.Column(db.DateTime, nullable=False)
+    within_diet = db.Column(db.Boolean, nullable=False)
